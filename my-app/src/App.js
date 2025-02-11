26fe96cad580bb8f510d3f657e0954da86df9381
Vasu <vasupothuraju17@gmail.com>
2025-02-11 12:17:46 +0530

    app changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Welcome to My App</h1>
     </div>
   );
 }
